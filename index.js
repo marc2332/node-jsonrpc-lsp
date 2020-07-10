f5725685cd4dc4c3296ce7f7974b4af200a20cb7
Marc Espín Sanz <mespinsanz@gmail.com>
2020-07-10 11:35:07 +0200

    Improvements

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const fs = require("fs")
 const ws = require("ws")
-const rpc = require("@sourcegraph/vscode-ws-jsonrpc")
 const rpcServer = require("@sourcegraph/vscode-ws-jsonrpc/lib/server")
+
+const DEFAULT_PORT = 3000
 
 function toSocket(webSocket) {
 	return {
@@ -19,36 +19,36 @@ function toSocket(webSocket) {
 
 
 function nodeJSONRPC({ languageServers, port }){
-
-	let serverPort = port || 3000;
+	const serverPort = port || DEFAULT_PORT
 
 	const wss = new ws.Server({
 		port: serverPort,
 		perMessageDeflate: false
-	});
-	
+	})
+
 	wss.on('connection', (client, request ) => {
 		let langServer;
-
 		Object.keys(languageServers).filter( key => {
 			if ( request.url === `/${key}` ) {
 				langServer = languageServers[key];
 			}
 		});
-		
 		if ( !langServer || !langServer.length ){
 			return client.close();
 		}
-		
 		let localConnection = rpcServer.createServerProcess('jsonrpc', langServer[0], langServer.slice(1));
 		let socket = toSocket(client);
 		let connection = rpcServer.createWebSocketConnection(socket);
 		rpcServer.forward(connection, localConnection);
-		
 		socket.onClose((code, reason) => {
-			localConnection.dispose();
-		});
-	});
+			localConnection.dispose()
+		})
+	})
+	return {
+		addLanguageServer(name, args){
+			languageServers[name] = args
+		}
+	}
 }
 
 module.exports = nodeJSONRPC
--- readme.md
+++ readme.md
@@ -1,1 +1,15 @@
 based on https://github.com/wylieconlon/jsonrpc-ws-proxy
+
+## ✍🏻 Usage
+
+```javascript
+const jsonRPC = require('node-jsonrpc-lsp')
+
+jsonRPC({
+	port: 2020,
+	languageServers: {
+		javascript: 'my-javascript-langserver.js'
+	}
+})
+
+```
